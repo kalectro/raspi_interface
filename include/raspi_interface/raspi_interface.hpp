1fe20daf764d38026edf4ba61f18f04d6be28d7f
Kai Franke <franke@fzi.de>
2013-07-31 12:17:30 +0200

    initial commit, not tested

---
+++ include/raspi_interface/raspi_interface.hpp
@@ -0,0 +1,110 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2012, Robert Bosch LLC.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Robert Bosch nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************/
+
+//\Author Kai Franke
+
+#ifndef RASPI_INTERFACE_H_
+#define RASPI_INTERFACE_H_
+
+#include <ros/ros.h>
+#include <wiringPi.h>
+
+#include <bosch_drivers_hardware_interface.hpp>
+#include <bosch_drivers_parameters.hpp>
+
+class RaspiInterface: public bosch_hardware_interface
+{
+  /**
+   * \brief The Raspberry Pi hardware interface.
+   * This code is meant to run on a raspberry pi with the wiringPi package installed
+   */
+
+public:
+ 
+  RaspiInterface();
+
+  ~RaspiInterface();
+
+ 
+  bool initialize();
+
+  /**
+   * \brief Reads \a num_bytes from the requested device on the specified \a protocol at the specified protocol \a frequency
+   * \var   int device_address the way that the sensor identifies itself
+   * \var   interface_protocol protocol the defined protocol
+   * \var   int frequency the frequency of the interface protocol
+   * \var   int* flags additional information necessary to read from that particular interface protocol.
+   * \var   uint8_t reg_address the starting address of the data in the sensor.
+   * \var   uint8_t* data the name of the array where the data will be stored.
+   * \var   uint8_t num_bytes the number of bytes to be read from the sensor.
+   * \return \a num_bytes or a value less than zero, if the read failed.
+   */
+  ssize_t read( int device_address, interface_protocol protocol, int frequency, int* flags, uint8_t reg_address, uint8_t* data, size_t num_bytes );
+  
+  /**
+   * \brief Writes \a num_bytes from the requested device on the specified \a protocol at the specified protocol \a frequency
+   * \var   int device_address the way that the sensor itentifies itself
+   * \var   interface_protocol protocol the defined protocol
+   * \var   int frequency the frequency of the interface protocol
+   * \var   int* flags additional information necessary to write to that particular interface protocol.
+   * \var   uint8_t reg_address the starting address in the sensor's registers where the data will be written to.
+   * \var   uint8_t* data the name of the array where the data will be output from.
+   * \var   uint8_t num_bytes the number of bytes to be written to the sensor.
+   * \return \a num_bytes or a value less than zero, if the write failed.
+   */
+  ssize_t write( int device_address, interface_protocol protocol, int frequency, int* flags, uint8_t reg_address, uint8_t* data, size_t num_bytes );
+
+  /**
+   * \brief  Returns true if the input protocol is supported by the hardware interface.
+   * \param  interface_protocol protocol the input protocol.
+   * \return true, if the hardware interface supports reading and writing on that particular protocol.
+   */
+  bool supportedProtocol( interface_protocol protocol );
+  
+  /**
+   * \return  the way the sensor identifies itself
+   */
+  std::string getID();
+
+private:
+
+  ssize_t raspiGpioWrite( uint8_t pin, bool value );
+  
+  bool is_initialized_;
+  
+};
+#endif //RASPI_INTERFACE_H_
+
---
+++ src/raspi_interface.cpp
@@ -0,0 +1,181 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013, Robert Bosch LLC.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Robert Bosch nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************/
+
+//\Author Kai Franke
+
+#include "raspi_interface/raspi_interface.hpp"
+
+/**********************************************************************/
+// Constructor
+/**********************************************************************/
+RaspiInterface::RaspiInterface( std::string port_name ):
+  is_initialized_( false )
+{
+}
+
+
+/**********************************************************************/
+// Destructor
+/**********************************************************************/
+RaspiInterface::~RaspiInterface()
+{
+}
+
+
+/**********************************************************************/
+// Initialize:   
+/**********************************************************************/
+bool RaspiInterface::initialize()
+{
+  // Prevent Double-initialization!
+  if( is_initialized_ == true )
+  {
+    ROS_INFO( "RaspiInterface::initialize(): WiringPi already initialized." );
+    return true;
+  }
+
+  // Setup WiringPi
+  int init_reply = wiringPiSetup ();
+  
+  if( init_reply != 0 )
+  {
+    ROS_ERROR( "WiringPi not initialized properly. Returned %i", init_success);
+    return false;
+  }
+
+  is_initialized_ = true;
+  ROS_INFO( "RaspiInterface::initialize(): WiringPi initialized." );
+  return true;
+}
+
+
+/**********************************************************************/
+// Read
+/**********************************************************************/
+ssize_t RaspiInterface::read( int device_address, interface_protocol protocol, int frequency, int* flags, uint8_t reg_address, uint8_t* data, size_t num_bytes )
+{
+  int error_code = 0;
+  
+  // Check initialization:
+  if( is_initialized_ == false )
+  {
+    return -1;
+  }
+ 
+  switch( protocol )
+  {
+    default:
+    {
+      ROS_ERROR("Raspberry Pi does not support reading through this protocol.");
+      return -1;
+    }
+  }
+  return error_code;
+}
+
+
+/**********************************************************************/
+// Write
+/**********************************************************************/
+ssize_t RaspiInterface::write( int device_address, interface_protocol protocol, int frequency, int* flags, uint8_t reg_address, uint8_t* data, size_t num_bytes )
+{ 
+  int error_code = 0;
+
+  // Check initialization:
+  if( is_initialized_ == false )
+  {
+    return -1;
+  }
+ 
+  switch( protocol )
+  {
+    case GPIO:
+    {
+      error_code = raspiGpioWrite( reg_address, (bool)data[0] );
+      break;
+    }
+    default:
+    {
+      ROS_ERROR( "Raspberry Pi does not support writing through this protocol." );
+      error_code = -1;
+    }
+  }
+ 
+  return error_code; // bytes written, or error. 
+}
+
+
+/**********************************************************************/
+// supportedProtocol
+/**********************************************************************/
+bool RaspiInterface::supportedProtocol( interface_protocol protocol )
+{
+  switch( protocol )
+  {
+  case GPIO:
+    return true;
+  default:
+    return false;
+  }
+}
+
+
+/**********************************************************************/
+std::string RaspiInterface::getID()
+/**********************************************************************/
+{
+  return "Raspberry Pi";
+}
+
+
+/**********************************************************************/
+/**********************************************************************/
+ssize_t RaspiInterface::raspiGpioWrite( uint8_t pin, bool value )
+{
+
+  // check if selected pin is valid for Raspberry Pi
+  if(pin < 0 || pin > 16)
+  {
+    ROS_ERROR("The selected Pin number is not available for GPIO");
+    ROS_ERROR("Select Pins 0 through 16 instead");
+    return -1;
+  }
+  
+  pinMode (pin, OUTPUT);
+  digitalWrite (pin, value);
+  
+  return 1;
+}
